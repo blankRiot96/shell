843222ca147cf73a6a754a0689026a924afc0b53
Axis <blankRiot96@gmail.com>
2026-03-02 19:58:23 +0530

    changed tokenization method & added tests
    switched to having a `TokenType` enum and `Token` dataclass
    switched to iterating over each character as opposed to whitespace
    new test `test_queue_mechanism` to see if pipe is correctly being added after the previous segment is parsed

--- src/shell/parser/tokenize.py
+++ src/shell/parser/tokenize.py
@@ -1,32 +1,61 @@
-from abc import ABC
+from collections import deque
 from dataclasses import dataclass
+from enum import Enum, auto
 
 
-BUILTIN_COMMAND_NAMES = (
-    "echo"
-)
+def string_to_bytearray(string: str) -> bytearray:
+    return bytearray(map(ord, string))
 
-class Token(ABC):
-    pass
+
+BUILTIN_COMMAND_NAMES = ("echo", "select", "sort", "ls")
+BUILTIN_COMMAND_NAMES = tuple(map(string_to_bytearray, BUILTIN_COMMAND_NAMES))
+
+
+class TokenType(Enum):
+    BUILTIN_COMMAND = auto()
+    STRING_LITERAL = auto()
+    COLUMN_NAME = auto()
+    SINGLE_DASH_FLAGS = auto()
+    DOUBLE_DASH_FLAGS = auto()
+    ARGUMENT = auto()
+    PIPE = auto()
+
+    EOF = auto()
+
 
 @dataclass
-class BuiltinCommandToken(Token):
-    command_name: str
-
-
-@dataclass
-class StringLiteralToken(Token):
-    data: str
+class Token:
+    type: TokenType
+    value: bytearray
 
 
 def create_tokens_from_code(code: str) -> list[Token]:
     tokens = []
-    for segment in code.split():
-        segment = segment.strip()
-        if segment in BUILTIN_COMMAND_NAMES:
-            tokens.append(BuiltinCommandToken(segment))
-        elif segment.startswith('"'):
-            data = segment.removeprefix('"').removesuffix('"')
-            tokens.append(StringLiteralToken(data))
+    segment = bytearray()
+    queue: deque[Token] = deque()
+    for char in code + " ":
+        if char == "|":
+            queue.append(Token(TokenType.PIPE, string_to_bytearray("|")))
+        elif char != " ":
+            segment.append(ord(char))
+            continue
 
+        if segment in BUILTIN_COMMAND_NAMES:
+            tokens.append(Token(TokenType.BUILTIN_COMMAND, segment))
+        elif segment.startswith(b'"'):
+            tokens.append(Token(TokenType.STRING_LITERAL, segment))
+        elif segment.startswith(b"."):
+            tokens.append(Token(TokenType.COLUMN_NAME, segment))
+        elif segment.startswith(b"--"):
+            tokens.append(Token(TokenType.DOUBLE_DASH_FLAGS, segment))
+        elif segment.startswith(b"-"):
+            tokens.append(Token(TokenType.SINGLE_DASH_FLAGS, segment))
+        else:
+            tokens.append(Token(TokenType.ARGUMENT, segment))
+
+        tokens.extend(queue)
+        queue.clear()
+        segment = bytearray()
+
+    tokens.append(Token(TokenType.EOF, bytearray()))
     return tokens
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,13 +1,25 @@
-from shell.parser.tokenize import (
-    create_tokens_from_code,
-    BuiltinCommandToken,
-    StringLiteralToken,
-)
 import pytest
+
+from shell.parser.tokenize import Token, TokenType, create_tokens_from_code
+from shell.parser.tokenize import string_to_bytearray as stb
 
 
 def test_builtin_command():
     assert create_tokens_from_code('echo "hii"') == [
-        BuiltinCommandToken("echo"),
-        StringLiteralToken("hii"),
+        Token(TokenType.BUILTIN_COMMAND, stb("echo")),
+        Token(TokenType.STRING_LITERAL, stb('"hii"')),
+        Token(TokenType.EOF, bytearray()),
+    ]
+
+
+def test_queue_mechanism():
+    assert create_tokens_from_code("ls|select .date|sort desc") == [
+        Token(TokenType.BUILTIN_COMMAND, stb("ls")),
+        Token(TokenType.PIPE, stb("|")),
+        Token(TokenType.BUILTIN_COMMAND, stb("select")),
+        Token(TokenType.COLUMN_NAME, stb(".date")),
+        Token(TokenType.PIPE, stb("|")),
+        Token(TokenType.BUILTIN_COMMAND, stb("sort")),
+        Token(TokenType.ARGUMENT, stb("desc")),
+        Token(TokenType.EOF, bytearray()),
     ]
